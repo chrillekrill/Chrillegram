3bc54c6faf35193648ad31d8c97b0a1405db5f74
korell christoffer <korellchristoffer@gmail.com>
2023-05-03 18:28:32 +0200

    Added controller

---
+++ ChrilleGram.Api/Controllers/ImageController.cs
@@ -0,0 +1,24 @@
+﻿using ChrilleGram.Api.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChrilleGram.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ImageController : Controller
+    {
+        private readonly Context _context;
+        public ImageController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> IndexAsync()
+        {
+            var images = await _context.Image.ToListAsync();
+            return Ok();
+        }
+    }
+}
